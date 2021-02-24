037d8e51f0135ff9ba49470041043d14b26cc297
Kristopher Teehan <kteehan@linkedin.com>
2021-02-24 13:41:35 -0500

    bug fix

--- src/calc_test.py
+++ src/calc_test.py
@@ -13,7 +13,7 @@ class TestStringMethods(unittest.TestCase):
 
     def test_sub_3arg(self):
         # Make sure 4 - 3 - 7 = -6
-        self.assertEqual(sub(4, 3, -7), -6, 'subtracting three from four')
+        self.assertEqual(sub(4, 3, 7), -6, 'subtracting three from four')
 
 if __name__ == '__main__':
     unittest.main()
